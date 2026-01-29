7ca3afde4ceb3e7a3ba56eacb9de94df602cfd4b
VicePO1 <viclindo11@gmail.com>
2026-01-29 20:07:20 +0000

    Fix

--- Meu_Projeto/GUI/buttons.py
+++ Meu_Projeto/GUI/buttons.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+from Meu_Projeto.Voz.reconhecer_voz import identificar_inicial
+from Meu_Projeto.Utils.utils import listen
 
-def create_button_listen():
-    button_listen=tk.Button(width=18,height=4,text="Listen",font=("Georgia",12))
+def create_button_listen(jan):
+    button_listen=tk.Button(jan,width=18,height=4,text="Listen",font=("Georgia",12),command=lambda:identificar_inicial(listen()))
     button_listen.place(x=115,y=100)
--- Meu_Projeto/main.py
+++ Meu_Projeto/main.py
@@ -1,5 +1,3 @@
 from Meu_Projeto.GUI.janela import janela_inicial
-from Meu_Projeto.Voz.reconhecer_voz import identificar_inicial
-from Utils.utils import listen
 
 janela_inicial()
