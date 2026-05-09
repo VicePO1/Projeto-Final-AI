0f8eb79f0affff70dbbc58905cdf1752a9c5e4e6
VicePO1 <viclindo11@gmail.com>
2026-05-09 14:52:06 +0100

    Update whatsapp.py

--- Meu_Projeto/Imagens/whatsapp.py
+++ Meu_Projeto/Imagens/whatsapp.py
@@ -2,3 +2,6 @@ import pywhatkit as what
 
 def enviar_imagem(cont,image):
     what.sendwhats_image(cont,image)
+
+def enviar_imagen_caption(cont,imagen,caption):
+    what.sendwhats_image(cont,imagen,caption)
